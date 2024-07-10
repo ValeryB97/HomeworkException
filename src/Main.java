5f9d917f5122a015e60631b4e7aa0de6d564143b
ValeryB97 <valery.babaryko@gmail.com>
2024-07-10 23:56:08 +0700

    Homework Exception.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
+    private static final String VALID_SYMBOLS = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890_";
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        try {
+            authenticationUser("123uuyO", "32r2f", "32r2f");
+        } catch (WrongLoginException wle) {
+            System.out.println("Ошибка в логине");
+        } catch (WrongPasswordException wpe) {
+            System.out.println("Ошибка в пароле");
+        }
+    }
+
+    private static void authenticationUser(String login, String password, String confirmPasswordS) {
+        for (int i = 0; i < login.length(); i++)
+            if (!VALID_SYMBOLS.contains(Character.toString(login.charAt(i))) || login.length() > 20) {
+                throw new WrongLoginException();
+            }
+        for (int i = 0; i < password.length(); i++) {
+            if (!VALID_SYMBOLS.contains(Character.toString(password.charAt(i))) || password.length() > 20) {
+                throw new WrongPasswordException();
+            }
+            if (!password.equals(confirmPasswordS)) {
+                throw new WrongPasswordException();
+            }
+        }
     }
 }
